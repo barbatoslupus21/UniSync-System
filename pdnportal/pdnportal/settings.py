3943f51a5f1bcec7443baf7964f05f255daecf95
zn <zenmier30@gmail.com>
2025-05-06 10:46:45 +0800

    docker config 2.0

--- pdnportal/pdnportal/settings.py
+++ pdnportal/pdnportal/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-3oti(#notk7n_)c&kid2p&@d$9pb!znyy22#jaryu3)@hiq8y5'
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
